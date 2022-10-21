56bc3e907632192c3f9796742824ec74a00a3329
roxas <jeropastrana@gmail.com>
2022-10-21 16:11:43 +0200

    Hasta aquí se enseña cómo se crea y escribe un archivo

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,11 @@
 import java.io.BufferedWriter;
 import java.io.File;
+import java.io.FileNotFoundException;
 import java.io.PrintWriter;
 import java.util.Scanner;
 
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws FileNotFoundException {
         Scanner scanner = new Scanner(System.in);
         String ficheroName = scanner.nextLine();
 
@@ -12,5 +13,12 @@ public class Main {
         PrintWriter pw = new PrintWriter(fichero);
 
         String linea;
+
+        while (!(linea = scanner.nextLine()).isEmpty()) {
+            pw.println(linea);
+        }
+
+        pw.close();
+
     }
 }
